2491d0f9a6ed5fd81614a62a841717fce086b3ed
Lucas Vieira <lucas.vieira.ext@nokia.com>
2023-10-25 06:56:24 -0300

    wip

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,12 +2,6 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
-  },
-  component: {
-    devServer: {
-      framework: 'next',
-      bundler: 'webpack',
-    },
+    baseUrl: 'https://www.google.com.br',
   },
 })
